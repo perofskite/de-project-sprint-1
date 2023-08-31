7cd6e4bbefed6224804683f179d86d9776ed282a
perofskite <109538100+perofskite@users.noreply.github.com>
2023-08-31 16:32:15 +0300

    Update tmp_rfm_recency.sql
    update after review

--- tmp_rfm_recency.sql
+++ tmp_rfm_recency.sql
@@ -1,13 +1,10 @@
 -- вычисляем фактор Recency
 insert into analysis.tmp_rfm_recency
 with tab1 as (
-select * from production.orderstatuslog o left join production.orderstatuses o2 
-on o.status_id = o2.id 
-left join production.orders o3 on o.order_id = o3.order_id 
-left join production.users u on o3.user_id = u.id
-where o2."key" = 'Closed' and o.dttm >= '2022-01-01'),
+select * from  analysis.orders o3 
+where order_ts >= '2022-01-01'),
 tab2 as (
-select user_id, max(dttm) as last_order
+select user_id , max(order_ts) as last_order
 from tab1
 group by user_id)
 select user_id, ntile(5) over (order by last_order) as Recency from tab2
