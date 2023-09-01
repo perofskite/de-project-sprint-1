6e49337b9608bfadbe9a25d655dbff9dd8738dbc
perofskite <109538100+perofskite@users.noreply.github.com>
2023-09-01 15:09:44 +0300

    Update tmp_rfm_recency.sql

--- tmp_rfm_recency.sql
+++ tmp_rfm_recency.sql
@@ -1,10 +1,10 @@
--- вычисляем фактор Recency
-insert into analysis.tmp_rfm_recency
-with tab1 as (
-select * from  analysis.orders o3 
-where order_ts >= '2022-01-01'),
-tab2 as (
-select user_id , max(order_ts) as last_order
+-- вычисляем фактор Recency 
+insert into analysis.tmp_rfm_recency 
+with tab1 as 
+(select o.user_id, max(case when o2.key = 'Closed' then order_ts::timestamp else null end) as last_order from analysis.orders o 
+left join analysis.orderstatuses o2 on o.status = o2.id 
+where order_ts >= '2022-01-01'
+group by o.user_id)
+select 
+user_id, NTILE(5) over (order by last_order::timestamp desc) as recency
 from tab1
-group by user_id)
-select user_id, ntile(5) over (order by last_order) as Recency from tab2
